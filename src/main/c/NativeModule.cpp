a0136d97ee3ba8da38b74493c49484ab90d8852a
Richard Cownie <richard@physicsspeed.com>
2018-05-07 10:26:43 -0400

    find linux g++ in /usr/bin

--- src/main/c/NativeModule.cpp
+++ src/main/c/NativeModule.cpp
@@ -164,7 +164,7 @@ public:
       javaHome_ = "/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home";
       javaMD_ = "darwin";
     } else {
-      llvmHome_ = lastMatch("/usr/l*/llvm* /usr/l*/llvm-5* /usr/l*/llvm-6*");
+      llvmHome_ = lastMatch("/usr /usr/l*/llvm* /usr/l*/llvm-5* /usr/l*/llvm-6*");
       cxxName_ = (fileExists(llvmHome_+"/bin/clang") ? "clang" : "g++");
       if (cxxName_ == "g++") llvmHome_ = "/usr";
       javaHome_ = "/usr/lib/jvm/default-java";
