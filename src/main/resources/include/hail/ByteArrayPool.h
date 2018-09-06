b4459e5f44ed45017d52ecb63c6e518a9cd5c8c6
Richard Cownie <rcownie@broadinstitute.org>
2018-09-06 13:54:02 -0400

    snapshot

---
+++ src/main/resources/include/hail/ByteArrayPool.h
@@ -0,0 +1,39 @@
+#ifndef HAIL_BYTEARRAYPOOL_H
+#define HAIL_BYTEARRAYPOOL_H 1
+
+#include <jni.h>
+#include <memory>
+
+// To use java.io.InputStream, we have to read into Scala Array[Byte] buffers.
+// But we want to recycle these buffers promptly rather than allowing them to
+// be garbage-collected, so we manage them
+
+namespace hail {
+
+class ByteArray {
+ public:
+  ssize_t capacity_;
+  jbyteArray array_;
+
+ public:
+  ByteArray(ssize_t capacity, jbyteArray array) : capacity_(capacity), array_(array) { }
+  
+  ByteArray(const ByteArray& b) : capacity_(b.capacity_), array_(b.array_) { }
+  
+  ByteArray& operator=(const ByteArray& b) {
+    capacity_ = b.capacity_;
+    array_ = b.array_;
+    return *this;
+  }
+
+  // Destructor is called when there no more refs, will save the array for reuse
+  ~ByteArray();
+};
+
+using ByteArrayPtr = std::shared_ptr<ByteArray>;
+
+ByteArrayPtr alloc_byte_array(ssize_t min_capacity);
+
+}
+
+#endif
